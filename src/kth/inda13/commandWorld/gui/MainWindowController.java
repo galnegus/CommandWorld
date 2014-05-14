8282d31cbc278604ced49024b8217f03c8745015
RodrigoRoaR <rorr@kth.se>
2014-05-14 13:43:58 +0200

    Added a quit command.

--- src/kth/inda13/commandWorld/gui/MainWindowController.java
+++ src/kth/inda13/commandWorld/gui/MainWindowController.java
@@ -87,25 +87,21 @@ public class MainWindowController {
 	@FXML
 	private void onSubmit() {
 		String input = inputTextField.getText();
-		String response = parser.Parse(input);
+		String response;
 		if (!input.isEmpty()) {
+			// Quit command
+			if(input.toLowerCase().equals("quit")) System.exit(0);
+
 			// remove old input
 			inputTextField.clear();
+
+			//Parse text and get reply
+			response = parser.Parse(input);
 
 			// print response & input
 			inputLabel.setText(input);
 			responseLabel.setText(response);
-			
-//			// hardcoded descriptions test
-//			if (input.equals("e small i red left person")) {
-//				world.event(world.get(Word.PERSON, Arrays.asList(Word.RED, Word.LEFT)), Word.SMALL);
-//			}
-//			if (input.equals("a monkey e eat i tiger")) {
-//				world.eat(world.get(Word.MONKEY), world.get(Word.TIGER));
-//			}
-//			if (input.equals("e size")) {
-//				world.printSize();
-//			}
+
 		}
 
 		// this will return focus to the textfield if the submit button was used rather than text area + ENTER
